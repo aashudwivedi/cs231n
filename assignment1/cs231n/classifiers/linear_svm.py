96e25c789459c59090ac51d2df98cd19d0d0c81e
Ashutosh Dwivedi <adwivedi@akamai.com>
2017-12-01 12:22:39 +0530

    added vetorized svm loss and gradient calculatino

---
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,111 @@
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+import pdb
+
+def svm_loss_naive(W, X, y, reg):
+  """
+  Structured SVM loss function, naive implementation (with loops).
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  # compute the loss and the gradient
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  delta = 1.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    for j in xrange(num_classes):
+      if j == y[i]:
+        continue
+      margin = scores[j] - correct_class_score + delta
+      if margin > 0:
+        loss += margin
+        dW[:, j] += X[i].T # if margin is > 0 gradient is 1 * X
+        dW[:, y[i]] -= X[i].T #
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
+  return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+  """
+  Structured SVM loss function, vectorized implementation.
+
+  Inputs and outputs are the same as svm_loss_naive.
+  """
+  loss = 0.0
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+  delta = 1.0
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the structured SVM loss, storing the    #
+  # result in loss.                                                           #
+  #############################################################################
+  n = X.shape[0]
+  scores = X.dot(W) # N X C
+  correct_cls_scores = scores[np.arange(n), y]
+  score_diff = scores - correct_cls_scores.reshape(n, 1)
+  score_diff[np.arange(n), y] = -1
+  margin = np.maximum(0,  score_diff + delta)
+  loss = margin.sum()
+
+  loss /= n
+  loss +=  reg * np.sum(W * W) # regularization
+
+  ##################d###########################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the gradient for the structured SVM     #
+  # loss, storing the result in dW.                                           #
+  #                                                                           #
+  # Hint: Instead of computing the gradient from scratch, it may be easier    #
+  # to reuse some of the intermediate values that you used to compute the     #
+  # loss.                                                                     #
+  #############################################################################
+  non_zero_idxs = margin > 0
+  margin[non_zero_idxs] = 0
+  margin[:,y] = -1 * margin.sum(axis=1)
+  dW = X.T.dot(margin)
+  dW / n
+  dW += reg * W
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+  return loss, dW
---
+++ assignment1/cs231n/classifiers/test_knn.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import numpy as np
+import k_nearest_neighbor
+
+
+def main():
+    train = np.arange(10).reshape(5, 2)
+    test = np.arange(6).reshape(3, 2)
+    print('train:')
+    print(train)
+    print('test')
+    print(test)
+
+
+main()
+
